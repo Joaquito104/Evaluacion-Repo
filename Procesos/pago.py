34276dc66e8232a2e6d4ec95eb5836582eb7a328
mirta-a <mirtaaguilarp@gmail.com>
2024-11-28 23:09:07 -0300

    Hotel

---
+++ Procesos/pago.py
@@ -0,0 +1,13 @@
+class Pago:
+    def __init__(self, id, monto, FechaPago, MetodoPago):
+        self.id = id
+        self.monto = monto
+        self.FechaPago = FechaPago
+        self.MetodoPago = MetodoPago
+
+    
+    def ProcesarPago(self):
+        
+        pass
+    
+  
---
+++ Procesos/reserva.py
@@ -0,0 +1,17 @@
+class Reserva:
+    def __init__(self, id, FechaInicio, FechaFin, Estado):
+        self.id = id
+        self.FechaInicio = FechaInicio 
+        self.FechaFin = FechaFin    
+        self.Estado = Estado
+
+    
+    def RealizaReserva(self):
+        pass
+    
+    def CancelaReserva(self):
+        pass
+    
+ 
+    
+   
