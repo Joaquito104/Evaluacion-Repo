80e5ca79c69581a47f92036b7ac1d04e3a869c2b
Joaquito <joaquin.rivera14@inacap.mail.cl>
2024-11-28 23:18:24 -0300

    falto habitaciones

---
+++ Hotelsito/habitacion.py
@@ -0,0 +1,10 @@
+class Habitacion:
+    def __init__(self, id, numero, tipo, precio, disponibilidad):
+        self.id = id
+        self.numero = numero
+        self.tipo = tipo
+        self.precio = precio
+        self.disponibilidad = disponibilidad
+
+    def disponibilidad():
+        pass #Añadir codigo
